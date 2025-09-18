d03e37290534125c26b9a9a092f82dcf2054fd4f
Nguyễn Huy Thắng <46878854+huythang304@users.noreply.github.com>
2025-09-18 23:51:38 +0700

    Add files via upload

---
+++ config/settings.php
@@ -0,0 +1,32 @@
+<?php
+
+// config for Inerba/DbConfig
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Table Name
+    |--------------------------------------------------------------------------
+    |
+    | The name of the database table used to store the settings.
+    |
+    */
+    'table_name' => 'settings',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Cache Configuration
+    |--------------------------------------------------------------------------
+    |
+    | Configure the cache behavior for the package. You can set a custom
+    | prefix for all cache keys and define a Time To Live (TTL) in minutes.
+    |
+    | Setting 'ttl' to null or 0 will cache the settings forever.
+    |
+    */
+    'cache' => [
+        'prefix' => 'settings',
+        'ttl' => null, // Cache forever by default
+    ],
+
+];
