31064f4f20e3166a763fa4efa8cbab05456c1f19
Nguyễn Huy Thắng <nhthang304@gmail.com>
2025-09-26 22:16:09 +0700

    Update settings.php

--- resources/lang/vi/settings.php
+++ resources/lang/vi/settings.php
@@ -1,10 +1,12 @@
 <?php
 
 return [
+
     'navigation_group'  =>  'Settings',
 
     // Notifications and buttons from AbstractPageSettings
     'saved_title' => 'Saved',
     'saved_body' => 'Settings have been saved successfully.',
     'save' => 'Save',
+    
 ];
