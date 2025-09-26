0a9186c4d3b2fbf1512c890c5eb1622f0d21df2c
Nguyễn Huy Thắng <nhthang304@gmail.com>
2025-09-26 23:20:52 +0700

    Add SettingServiceProvider.php

---
+++ src/Providers/SettingServiceProvider.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace Nhwin304\Settings\Providers;
+
+use Illuminate\Filesystem\Filesystem;
+use Illuminate\Support\Facades\Blade;
+use Nhwin304\Settings\Commands\SettingCommand;
+use Nhwin304\Settings\Testing\TestsDbConfig;
+use Livewire\Features\SupportTesting\Testable;
+use Spatie\LaravelPackageTools\Commands\InstallCommand;
+use Spatie\LaravelPackageTools\Package;
+use Spatie\LaravelPackageTools\PackageServiceProvider;
+
+class SettingServiceProvider extends PackageServiceProvider
+{
+	public static string $name = 'settings';
+
+    public static string $viewNamespace = 'settings';
+
+    public function configurePackage(Package $package): void
+    {
+        /*
+         * This class is a Package Service Provider
+         *
+         * More info: https://github.com/spatie/laravel-package-tools
+         */
+        $package->name(static::$name)
+            ->hasConfigFile()
+	        ->hasMigrations($this->getMigrations())
+	        ->hasTranslations()
+	        ->hasCommands($this->getCommands())
+	        ->hasInstallCommand(function (InstallCommand $command) {
+	            $command
+	                ->publishConfigFile()
+	                ->publishTranslations()
+	                ->publishMigrations()
+	                ->askToRunMigrations()
+	                ->askToStarRepoOnGitHub('nhwin304/settings');
+	        });
+
+        $configFileName = $package->shortName();
+
+        if (file_exists($package->basePath("/../config/{$configFileName}.php"))) {
+            $package->hasConfigFile();
+        }
+
+        if (file_exists($package->basePath('/../database/migrations'))) {
+            $package->hasMigrations($this->getMigrations());
+        }
+
+        if (file_exists($package->basePath('/../resources/lang'))) {
+            $package->hasTranslations();
+        }
+
+    }
+
+    public function packageRegistered(): void {}
+
+    public function packageBooted(): void
+    {
+        // Handle Stubs
+        if (app()->runningInConsole()) {
+            foreach (app(Filesystem::class)->files(__DIR__ . '/../stubs/') as $file) {
+                $this->publishes([
+                    $file->getRealPath() => base_path("stubs/settings/{$file->getFilename()}"),
+                ], 'settings-stubs');
+            }
+        }
+
+        // Set the Blade directive to retrieve the settings
+        Blade::directive('settings', function ($expression) {
+            return "<?php echo \Nhwin304\Settings\Supports\Setting::get($expression); ?>";
+        });
+    }
+
+    protected function getAssetPackageName(): ?string
+    {
+        return 'nhwin304/settings';
+    }
+
+    /**
+     * @return array<class-string>
+     */
+    protected function getCommands(): array
+    {
+        return [
+            SettingCommand::class,
+        ];
+    }
+
+    /**
+     * @return array<string>
+     */
+    protected function getMigrations(): array
+    {
+        return [
+            'create_settings_table',
+        ];
+    }
+}
