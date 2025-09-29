2467b6f3dc597b95d4d6af8bafee9a1c1f3cc95b
Nguyễn Huy Thắng <nhthang304@gmail.com>
2025-09-29 23:01:29 +0700

    Update SettingServiceProvider.php

--- src/Providers/SettingServiceProvider.php
+++ src/Providers/SettingServiceProvider.php
@@ -46,7 +46,7 @@ class SettingServiceProvider extends PackageServiceProvider
     {
         // Handle Stubs
         if (app()->runningInConsole()) {
-            foreach (app(Filesystem::class)->files(__DIR__ . '/../stubs/') as $file) {
+            foreach (app(Filesystem::class)->files(__DIR__ . '/../../stubs/') as $file) {
                 $this->publishes([
                     $file->getRealPath() => base_path("stubs/settings/{$file->getFilename()}"),
                 ], 'settings-stubs');
